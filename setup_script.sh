010d7437bb60ba7e895501bf6f855de8e3d4b347
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2021-10-24 20:27:16 +0530

    Only use indian pines

--- setup_script.sh
+++ setup_script.sh
@@ -11,20 +11,22 @@ declare -a NETWORKS=("PyResNet"
 
 declare -a DATAFILES=("Indian_pines_corrected.mat"
                     "Indian_pines_gt.mat"
-                    "PaviaU.mat"
-                    "PaviaU_gt.mat"
-                    "KSC.mat"
-                    "KSC_gt.mat"
-                    "Salinas_corrected.mat"
-                    "Salinas_gt.mat")
+#                     "PaviaU.mat"
+#                     "PaviaU_gt.mat"
+#                     "KSC.mat"
+#                     "KSC_gt.mat"
+#                     "Salinas_corrected.mat"
+#                     "Salinas_gt.mat"
+)
 declare -a DATAURL=("http://www.ehu.eus/ccwintco/uploads/6/67/Indian_pines_corrected.mat"
                     "http://www.ehu.eus/ccwintco/uploads/c/c4/Indian_pines_gt.mat"
-                    "http://www.ehu.eus/ccwintco/uploads/e/ee/PaviaU.mat"
-                    "http://www.ehu.eus/ccwintco/uploads/5/50/PaviaU_gt.mat"
-                    "http://www.ehu.es/ccwintco/uploads/2/26/KSC.mat"
-                    "http://www.ehu.es/ccwintco/uploads/a/a6/KSC_gt.mat"
-                    "https://github.com/gokriznastic/HybridSN/raw/master/data/Salinas_corrected.mat"
-                    "https://github.com/gokriznastic/HybridSN/raw/master/data/Salinas_gt.mat")
+#                     "http://www.ehu.eus/ccwintco/uploads/e/ee/PaviaU.mat"
+#                     "http://www.ehu.eus/ccwintco/uploads/5/50/PaviaU_gt.mat"
+#                     "http://www.ehu.es/ccwintco/uploads/2/26/KSC.mat"
+#                     "http://www.ehu.es/ccwintco/uploads/a/a6/KSC_gt.mat"
+#                     "https://github.com/gokriznastic/HybridSN/raw/master/data/Salinas_corrected.mat"
+#                     "https://github.com/gokriznastic/HybridSN/raw/master/data/Salinas_gt.mat"
+)
 
 if [ ! -d "$DATASET_PATH" ]; then
   # Take action if $DIR exists. #
